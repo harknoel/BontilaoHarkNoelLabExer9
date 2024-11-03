fcdc4062537c14ec243b952602cd34a348a8f1e8
harknoel <bontilao.harknoel@gmail.com>
2024-11-03 22:26:19 +0800

    refactor(web): make a select component

---
+++ src/components/Select/SelectComponent.tsx
@@ -0,0 +1,45 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { EncodingScheme } from "@/types/encodingScheme";
+
+const SelectComponent = ({
+  selectedEncoding,
+  setSelectedEncoding,
+}: {
+  selectedEncoding: string;
+  setSelectedEncoding: (value: string) => void;
+}) => {
+  return (
+    <Select value={selectedEncoding} onValueChange={setSelectedEncoding}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select an encoding technique" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Encoding Techniques</SelectLabel>
+          <SelectItem value={EncodingScheme.NRZ_L}>NRZ - L</SelectItem>
+          <SelectItem value={EncodingScheme.NRZ_I}>NRZ - I</SelectItem>
+          <SelectItem value={EncodingScheme.BIPOLAR_AMI}>
+            Bipolar - AMI
+          </SelectItem>
+          <SelectItem value={EncodingScheme.PSEUDOTERNARY}>
+            Pseudoternary
+          </SelectItem>
+          <SelectItem value={EncodingScheme.MANCHESTER}>Manchester</SelectItem>
+          <SelectItem value={EncodingScheme.DIFFERENTIAL_MANCHESTER}>
+            Differential Manchester
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SelectComponent;
--- src/pages/Scheme/Scheme.tsx
+++ src/pages/Scheme/Scheme.tsx
@@ -1,59 +1,25 @@
-import { EncodingScheme } from "@/types/encodingScheme";
+import SelectComponent from "@/components/Select";
 import { LineCoding } from "@/utils/lineCoding";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { useEffect, useState } from "react";
 
 const Scheme = () => {
   const [selectedEncoding, setSelectedEncoding] = useState<string>("");
 
-  const handleOnChanged = (e: any) => {
-    setSelectedEncoding(e.target.value);
-  };
-
   useEffect(() => {
     console.log(selectedEncoding);
-    let input = [0, 1, 0, 0, 1, 1, 1, 0]
-    const result = LineCoding.convert(selectedEncoding, input);
-    console.log(result)
-
+    // let input = [0, 1, 0, 0, 1, 1, 1, 0];
+    // const result = LineCoding.convert(selectedEncoding, input);
+    // console.log(result);
   }, [selectedEncoding]);
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="max-w-4xl"></div>
       <div className="p-10">
-        <Select value={selectedEncoding} onValueChange={setSelectedEncoding}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select an encoding technique" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Encoding Techniques</SelectLabel>
-              <SelectItem value={EncodingScheme.NRZ_L}>NRZ - L</SelectItem>
-              <SelectItem value={EncodingScheme.NRZ_I}>NRZ - I</SelectItem>
-              <SelectItem value={EncodingScheme.BIPOLAR_AMI}>
-                Bipolar - AMI
-              </SelectItem>
-              <SelectItem value={EncodingScheme.PSEUDOTERNARY}>
-                Pseudoternary
-              </SelectItem>
-              <SelectItem value={EncodingScheme.MANCHESTER}>
-                Manchester
-              </SelectItem>
-              <SelectItem value={EncodingScheme.DIFFERENTIAL_MANCHESTER}>
-                Differential Manchester
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        <SelectComponent
+          selectedEncoding={selectedEncoding}
+          setSelectedEncoding={setSelectedEncoding}
+        ></SelectComponent>
       </div>
     </div>
   );
