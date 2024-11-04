92e22c5dd7fecb4438fe4751f9278bb3422e262c
harknoel <bontilao.harknoel@gmail.com>
2024-11-04 18:39:09 +0800

    feat(web): set NRZ - L as default

--- src/pages/Scheme/Scheme.tsx
+++ src/pages/Scheme/Scheme.tsx
@@ -3,27 +3,30 @@ import SelectComponent from "@/components/Select";
 import TableComponent from "@/components/Table";
 import { LineCoding } from "@/utils/LineCoding";
 import { SignalLevel } from "@/types/SignalLevel";
-import { BorderBuilder } from "@/utils/BorderBuilder";
+import { Input } from "@/components/ui/input";
+import { EncodingScheme } from "@/types/EncodingScheme";
 
 const Scheme = () => {
   let input = [0, 1, 0, 0, 1, 1, 1, 0, 1];
-  const [selectedEncoding, setSelectedEncoding] = useState<string>("");
+  const [selectedEncoding, setSelectedEncoding] = useState<string>(EncodingScheme.NRZ_L);
   const [result, setResult] = useState<SignalLevel[]>([]);
 
   useEffect(() => {
     const output = LineCoding.convert(selectedEncoding, input);
     setResult(output);
-    // console.log(result);
-    // const test = new BorderBuilder().up().left().build();
-    // console.log(test);
   }, [selectedEncoding]);
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="max-w-4xl">
-        <TableComponent scheme={selectedEncoding} input={input} result={result}></TableComponent>
+        <Input type="email" placeholder="Input" className="w-[200px] mb-10"/>
+        <TableComponent
+          scheme={selectedEncoding}
+          input={input}
+          result={result}
+        ></TableComponent>
       </div>
-      <div className="p-10">
+      <div className="m-10">
         <SelectComponent
           selectedEncoding={selectedEncoding}
           setSelectedEncoding={setSelectedEncoding}
