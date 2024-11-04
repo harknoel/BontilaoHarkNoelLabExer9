79e30ae1fc533dbd39a18b4a07c3e2ececd346ed
harknoel <bontilao.harknoel@gmail.com>
2024-11-04 16:55:22 +0800

    feat(web): add transition on the last

--- src/components/Table/TableComponent.tsx
+++ src/components/Table/TableComponent.tsx
@@ -45,7 +45,8 @@ const TableComponent = ({
                 ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 2 && result[index + 1] === 1 ? "border-r-4 border-r-red-500" : "") : ""}
                 ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 1 && result[index + 1] === 2 ? "border-r-4 border-r-red-500" : "") : ""}
                 ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 0 && result[index + 1] === 1 ? "border-r-4 border-r-red-500" : "") : ""}
-                ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 1 && result[index + 1] === 0 ? "border-r-4 border-r-red-500" : "") : ""}`}
+                ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 1 && result[index + 1] === 0 ? "border-r-4 border-r-red-500" : "") : ""}
+                ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 1 && index + 1 === result.length ? "border-r-4 border-r-red-500" : "") : ""}`}
               ></TableCell>
             </React.Fragment>
           ))}
@@ -70,7 +71,8 @@ const TableComponent = ({
                 ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 2 && result[index + 1] === 0 ? "border-r-4 border-r-red-500" : "") : ""}
                 ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 0 && result[index + 1] === 2 ? "border-r-4 border-r-red-500" : "") : ""}
                 ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 0 && result[index + 1] === 1 ? "border-r-4 border-r-red-500" : "") : ""}
-                ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 1 && result[index + 1] === 0 ? "border-r-4 border-r-red-500" : "") : ""}`}
+                ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 1 && result[index + 1] === 0 ? "border-r-4 border-r-red-500" : "") : ""}
+                ${scheme === EncodingScheme.BIPOLAR_AMI || EncodingScheme.PSEUDOTERNARY ? (result[index] === 0 && index + 1 === result.length ? "border-r-4 border-r-red-500" : "") : ""}`}
               ></TableCell>
             </React.Fragment>
           ))}
